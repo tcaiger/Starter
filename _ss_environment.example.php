8e3f3927cfb1a70fccfc967a9e1efd335d2bbc54
Tom Caiger <tom@swordfox.nz>
2017-04-03 12:03:13 +1200

    Add example ss env file

---
+++ _ss_environment.example.php
@@ -0,0 +1,14 @@
+<?php
+
+define('SS_DATABASE_SERVER', 'localhost');
+define('SS_DATABASE_USERNAME', 'XXXXXXXXXX');
+define('SS_DATABASE_PASSWORD', 'XXXXXXXXXXX');
+
+define('SS_ENVIRONMENT_TYPE', 'live');
+
+define('SS_DATABASE_CLASS', 'MySQLDatabase');
+
+define('SS_DEFAULT_ADMIN_USERNAME', 'admin');
+define('SS_DEFAULT_ADMIN_PASSWORD', '?qu3ry5tr1ng');
+
+// To Do: agree on a naming convention for databases and usernames
